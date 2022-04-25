631efacd6b1d506e5ef9dc4a5e69ff9ec32690f8
Vadim <1lavabox1@gmail.com>
2022-04-25 15:59:32 +0300

    Add project files.

---
+++ WebChatExam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebChatExam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
